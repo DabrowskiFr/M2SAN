e35d82526bf9567b4e2b0440f6e9969a9fd61681
Frédéric Dabrowski <33789275+DabrowskiFr@users.noreply.github.com>
2022-10-01 18:38:22 +0200

    add CameraApp

---
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.cameraapp;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.cameraapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int MY_CAMERA_PERMISSION_CODE = 1000;
+    ActivityMainBinding binding;
+    Button button;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        button = binding.button;
+        imageView = binding.imageView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Check if the system has a camera feature
+        if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+
+            // Check if the permission is already granted
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                // if not request the permission, the request is identified by MY_CAMERA_PERMISSION_CODE
+                // result handled by onRequestPermissionResult
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            }
+            else
+            {
+                // otherwise, take the picture
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                // Replace startActivityForResult, which is deprecated, see someActivityResultLauncher bellow
+                someActivityResultLauncher.launch(cameraIntent);
+            }
+        } else {
+            // No Camera
+            Toast.makeText(this, "no camera", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                // Replace startActivityForResult, which is deprecated, see someActivityResultLauncher bellow
+                someActivityResultLauncher.launch(cameraIntent);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Intent data  = result.getData();
+                        Bitmap photo = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(photo);
+                    }
+                }
+            }
+    );
+}
